2659c0844a17ad39b13c561cecdc52bc0a4285db
musakayretli <musakayretli06@gmail.com>
2022-10-17 11:29:04 +0300

    güncelleme yaptım

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,6 +2,8 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("musa kayretli");
         System.out.println("yaptım hocam");
+        System.out.println("güncelleme deneme ");
+
 
     }
 }
